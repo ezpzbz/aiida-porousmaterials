8c78f854a29dac878056a8c856ab6462926a32e9
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2019-08-08 13:17:09 +0200

    [WIP]-Calculation part

--- calculation/__init__.py
+++ calculation/__init__.py
@@ -1,1 +1,59 @@
+# -*- coding: utf-8 -*-
+""" PorousMaterials Calculation Plugin """
+# Importing system modules
 
+# Importing AiiDA modules
+
+
+# Coding the class
+
+class PorousMaterialsCalculation(CalcJob):
+    """
+    This is PorousMaterialsCalculation as the subclass
+    of AiiDA CalcJob to prepare input for the PorousMaterials
+    suite of Julia codes.
+    Please refer to : https://github.com/SimonEnsemble/PorousMaterials.jl
+    """
+    # Defaults
+    # TODO: Double thinking about these defaults.
+    INPUT_FILE = 'framework.jl'
+    OUTPUT_FOLDER = 'Output'
+    PROJECT_NAME = 'aiida'
+    DEFAULT_PARSER = 'porousmaterials'
+
+    @classmethod
+    def define(cls, spec):
+        """
+        The important section to define the class, inputs and outputs.
+        """
+        super(PorousMaterialsCalculation, cls).define(spec)
+
+        # Input parameters
+        # TODO: I should decide to choose one or make the
+        # code flexible so both can be used on the users choice.
+        spec.input('framework', valid_type=CifData,required=True,
+                    help='Framework input file as CIF')
+        spec.input('framework', valid_type=StructureData,required=True,
+                    help='Framework input file as CSSR')
+        spec.input('forcefiled', valid_type=SinglefileData, require=True,
+                    help='forcefiled parameters as csv file')
+        spec.input('parameters', valid_type=Dict, required=False,
+                    help='parameters such as cutoff and mixing rules.')
+                    
+
+
+        # Output parameters
+
+        # Exit codes
+
+        # Default output node
+
+
+    def prepare_for_submission(self, folder):
+        """
+        This is the routine to be called when you want to create
+        the input files and related stuff with a plugin.
+
+        :param folder: a aiida.common.folders.Folder subclass where
+                           the plugin should put all its files.
+        """
--- parser/__init__.py
+++ parser/__init__.py
@@ -1,1 +1,18 @@
+# -*- coding: utf-8 -*-
+"""PorousMaterials Output Parse"""
+# Importing system modules
 
+# Importing AiiDA modules
+
+class PorousMaterialsParser(Parser):
+    """
+    Parsing the PorousMaterials output.
+    """
+    def parse(self, **kwargs):
+        """
+        Receives in input a dictionary of retrieved nodes.
+        Does all the logic here.
+        """
+
+
+        return ExitCode(0)
