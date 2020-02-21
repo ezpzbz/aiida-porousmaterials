2d3da24e071ad6d18f7aeeec9fb394b5b26eb280
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-02-21 16:39:17 +0100

    [WIP] adding tests

---
+++ examples/simple_calculations/example_ev.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+""" Sample run script for PorousMaterials Plugin"""
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+import click
+
+from aiida.common import NotExistent
+from aiida.orm import Code, Dict
+from aiida.plugins import DataFactory
+from aiida.engine import run
+from aiida_porousmaterials.calculations import PorousMaterialsCalculation
+
+# Reading the structure and convert it to structure data.
+SinglefileData = DataFactory('singlefile')  # pylint: disable=invalid-name
+CifData = DataFactory('cif')  # pylint: disable=invalid-name
+
+def example_ev(julia_code, submit=True):
+    """
+    Example to prepare and run a Sinlge Component
+    """
+    try:
+        code = Code.get_from_string(codelabel)
+    except NotExistent:
+        print("The code '{}' does not exist".format(codelabel))
+        sys.exit(1)
+
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    framework = CifData(file=os.path.join(pwd, 'files', 'FIQCEN_clean.cif')).store()
+    acc_voronoi_nodes = SinglefileData(file=os.path.join(pwd, 'files', 'FIQCEN_clean.voro_accessible')).store()
+
+    parameters = Dict(
+        dict={
+            'data_path': "/storage/brno9-ceitec/home/pezhman/projects/noble_gas_epfl/xe_kr/data",
+            'ff': 'UFF.csv',
+            'cutoff': 12.5,
+            'mixing': 'Lorentz-Berthelot',
+            'framework': framework.filename,
+            'frameworkname': framework.filename[:-4],
+            'adsorbate': "Xe",
+            'temperature': 298.0,
+            'output_filename': "Ev_" + framework.filename[:-4] + ".csv",
+            'input_template': 'ev_vdw_1comp_template',
+            'ev_setting': [99, 95, 90, 80, 50],  # if not defined the default is [90,80,50]
+        }
+    )
+
+    builder = PorousMaterialsCalculation.get_builder()
+    builder.structure = {framework.filename[:-4]: framework}
+    builder.parameters = parameters
+    builder.acc_voronoi_nodes = {framework.filename[:-4]: acc_voronoi_nodes}
+    builder.code = code
+    builder.metadata.options.resources = { #pylint: disable = no-member
+        "num_machines": 1,
+        "num_mpiprocs_per_machine": 1,
+    }
+    builder.metadata.options.max_wallclock_seconds = 1 * 30 * 60  #pylint: disable = no-member
+    builder.metadata.options.withmpi = False  #pylint: disable = no-member
+
+    if submit:
+        print("Testing PorousMaterials with simple input...")
+        res, pk = run_get_pk(builder)
+        print("calculation pk: ", pk)
+        # TODO: Getting and showing results.
+        # print("Average number of methane molecules/uc:",
+        #       res['output_parameters'].dict.tcc1rs['components']['methane']['loading_absolute_average'])
+        print("OK, calculation has completed successfully")
+        pytest.base_calc_pk = pk
+    else:
+        print("Generating test input ...")
+        builder.metadata.dry_run = True  #pylint: disable = no-member
+        builder.metadata.store_provenance = False  #pylint: disable = no-member
+        run(builder)
+        print("submission test successful")
+        print("In order to actually submit, add '--submit'")
+    print("-----------")
+
+@click.command('cli')
+@click.argument('codelabel')
+@click.option('--submit', is_flag=True, help='Actually submit calculation')
+def cli(codelabel, submit):
+    """Click interface"""
+    try:
+        code = Code.get_from_string(codelabel)
+    except NotExistent:
+        print("The code '{}' does not exist".format(codelabel))
+        sys.exit(1)
+    example_ev(code, submit)
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
+
+# EOF
