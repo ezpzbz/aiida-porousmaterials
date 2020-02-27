9faac766be32ea791188782074ff28a565cc37b3
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-02-27 18:02:12 +0100

    enable tests

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
-# """
-# For pytest initialise a test database and profile
-# """
-# import pytest
-# pytest_plugins = ['aiida.manage.tests.pytest_fixtures']  # pylint: disable=invalid-name
-#
-#
-# @pytest.fixture(scope='function')
-# def julia_code(aiida_local_code_factory):  # pylint: disable=unused-argument
-#     return aiida_local_code_factory("julia")
+"""
+For pytest initialise a test database and profile
+"""
+import pytest
+pytest_plugins = ['aiida.manage.tests.pytest_fixtures']  # pylint: disable=invalid-name
+
+
+@pytest.fixture(scope='function')
+def julia_code(aiida_local_code_factory):  # pylint: disable=unused-argument
+    return aiida_local_code_factory("julia")
